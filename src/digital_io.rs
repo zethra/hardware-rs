018ac897054c05cb892cb7282c2b6218b684623d
Tim <timothyhollabaugh@gmail.com>
2017-08-05 15:56:50 -0400

    Put self in the digital methods

--- src/digital_io.rs
+++ src/digital_io.rs
@@ -5,10 +5,16 @@ pub enum PinState {
 }
 
 pub trait DigitalOutput<T> {
-    fn write(state: PinState);
-    fn read() -> PinState;
+
+    /// Write the pin high or low
+    fn write(&mut self, state: PinState);
+
+    /// Read the current state of the pin
+    fn read(&self) -> PinState;
 }
 
 pub trait DigitalInput<T> {
-    fn read() -> PinState;
+
+    /// Read the pin
+    fn read(&self) -> PinState;
 }
