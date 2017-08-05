8ff534763d8cf714727ed2019102e43bd4f885fc
Tim <timothyhollabaugh@gmail.com>
2017-08-05 16:22:37 -0400

    PinState => DigitalValue

--- src/digital_io.rs
+++ src/digital_io.rs
@@ -1,5 +1,6 @@
 
-pub enum PinState {
+#[derive(Debug)]
+pub enum DigitalValue {
     High = 1,
     Low = 0,
 }
@@ -7,14 +8,14 @@ pub enum PinState {
 pub trait DigitalOutput<T> {
 
     /// Write the pin high or low
-    fn write(&mut self, state: PinState);
+    fn write(&mut self, state: DigitalValue);
 
     /// Read the current state of the pin
-    fn read(&self) -> PinState;
+    fn read(&self) -> DigitalValue;
 }
 
 pub trait DigitalInput<T> {
 
     /// Read the pin
-    fn read(&self) -> PinState;
+    fn read(&self) -> DigitalValue;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 
+#![no_std]
+
 //! Generic hardware abstractions for microcontrollers
 
 pub mod pin;
