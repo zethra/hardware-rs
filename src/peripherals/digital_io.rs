a80b6ffbe4fdd1e5bb51781a122b79efe554e23d
Tim <timothyhollabaugh@gmail.com>
2017-08-09 19:16:00 -0400

    Not and From for DigitalValue

--- src/peripherals/digital_io.rs
+++ src/peripherals/digital_io.rs
@@ -1,8 +1,30 @@
+use core::ops::Not;
+use core::convert::From;
 
 #[derive(Debug, Clone, Copy)]
 pub enum DigitalValue {
     High = 1,
     Low = 0,
+}
+
+impl Not for DigitalValue {
+    type Output = DigitalValue;
+
+    fn not(self) -> DigitalValue {
+        match self {
+            DigitalValue::High => DigitalValue::Low,
+            DigitalValue::Low => DigitalValue::High,
+        }
+    }
+}
+
+impl From<bool> for DigitalValue {
+    fn from(other: bool) -> Self {
+        match other {
+            true => DigitalValue::High,
+            false => DigitalValue::Low,
+        }
+    }
 }
 
 pub trait DigitalOutput {
